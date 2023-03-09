58852b9d5ee4c8227e409c242ae9b0df5aff7901
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-03-09 18:14:07 +0300

    add task in todolist

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,19 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
+    const task1 = [
+        {id: 1, title: "HTML & CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false}
+    ]
+
+    const task2 = [
+        {id: 1, title: "Hello world", isDone: true},
+        {id: 2, title: "I am happy", isDone: false},
+        {id: 3, title: "Yo", isDone: false}
+    ]
+
+
     return (
         <div className="App">
             <Todolist title={'What to learn'}/>
