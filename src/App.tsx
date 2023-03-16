a9421b4280236ff41cb315a5963fc8aed080dbca
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-03-16 17:51:05 +0300

    add logic in removeTask for task

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,20 @@ export type TaskType = {
 }
 
 function App() {
-    const task1: TaskType[] = [
+    const task: TaskType[] = [
         {id: 1, title: "HTML & CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false}
     ]
 
     const removeTask = (taskId: number) => {
-        console.log(taskId)
+        task.filter(t => t.id !== taskId)
     }
 
 
     return (
         <div className="App">
-            <Todolist title={'What to learn'} tasks={task1} removeTask={removeTask}/>
+            <Todolist title={'What to learn'} tasks={task} removeTask={removeTask}/>
         </div>
     );
 }
