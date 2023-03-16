302eb3398be4cc645032bf827f2c8d56cf4f25ec
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-03-16 17:55:28 +0300

    created useState for task + fix logic in removeTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
@@ -9,14 +9,14 @@ export type TaskType = {
 }
 
 function App() {
-    const task: TaskType[] = [
+    const [task, setTask] = useState<TaskType[]>([
         {id: 1, title: "HTML & CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false}
-    ]
+    ])
 
     const removeTask = (taskId: number) => {
-        task.filter(t => t.id !== taskId)
+        setTask(task.filter(t => t.id !== taskId))
     }
 
 
