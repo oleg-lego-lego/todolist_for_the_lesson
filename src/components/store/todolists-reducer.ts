e4aa6f67a394de7661ff1eb3a2e62bba0925971e
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-04-07 14:24:52 +0300

    created case 'REMOVE-TODOLIST' for todolistsReducer

--- src/components/store/todolists-reducer.ts
+++ src/components/store/todolists-reducer.ts
@@ -1,12 +1,20 @@
 import {TodolistType} from "../../App";
 
-export const todolistsReducer = (state: TodolistType[], action: any): TodolistType[] => {
+export const todolistsReducer = (state: TodolistType[], action: ActionType): TodolistType[] => {
     switch (action.type) {
-        case '': {
-            return state
+        case 'REMOVE-TODOLIST': {
+            return state.filter(f => f.id !== action.todolistId)
         }
         default: {
             return state
         }
     }
+}
+
+export type  ActionType = removeTodolistACType
+
+
+type removeTodolistACType = ReturnType<typeof removeTodolistAC>
+export const removeTodolistAC = (todolistId: string) => {
+    return {type: 'REMOVE-TODOLIST', todolistId} as const
 }
