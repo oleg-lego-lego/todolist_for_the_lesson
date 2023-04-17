fa9a4594d075342c6b111b3764415f92740aa42f
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-04-17 14:49:06 +0300

    add link AppWithRedux

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppWithReducer from "./AppWithReducer";
 import {Provider} from "react-redux";
 import {store} from "./store/store";
+import AppWithRedux from "./AppWithRedux";
 
 
 const root = ReactDOM.createRoot(
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <AppWithReducer />
+    <AppWithRedux />
   </Provider>
 );
 
