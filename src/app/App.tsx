f37008902b328e268d20761ac36ebff88726c107
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-05-23 13:40:35 +0300

    add Routes for app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,7 @@ import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import { Menu } from '@mui/icons-material';
+import {Menu} from '@mui/icons-material';
 import LinearProgress from "@mui/material/LinearProgress";
 import TodoListsList from "../TodoList/TodoListsList";
 import {useSelector} from "react-redux";
@@ -14,6 +14,7 @@ import {AppRootStateType} from "./store";
 import {RequestStatusType} from "./app-reducer";
 import {ErrorSnackbar} from "../components/ErrorSnackbar /ErrorSnackbar";
 import {Login} from "../Login/Login";
+import {Navigate, Route, Routes} from "react-router-dom";
 
 function App() {
     const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
@@ -33,8 +34,12 @@ function App() {
             </AppBar>
             {status === 'loading' && <LinearProgress color={'secondary'}/>}
             <Container fixed>
-                <TodoListsList/>
-                <Login/> 
+                <Routes>
+                    <Route path={'/'} element={<TodoListsList/>}/>
+                    <Route path={'/login'} element={<Login/>}/>
+                    <Route path={'/404'} element={<h1>404: PAGE NOT FOUND</h1>}/>
+                    <Route path={'*'} element={<Navigate to={'/404'}/>}/>
+                </Routes>
             </Container>
             <ErrorSnackbar/>
         </div>
