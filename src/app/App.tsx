e78ace59cf206de05dd17c37b369e28fa37df536
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-12-06 18:35:37 +0300

    fix logic for component ButtonAppBar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,60 +1,36 @@
 import React, {useEffect} from 'react';
 import './App.css';
 import {CircularProgress, Container} from "@mui/material";
-import AppBar from "@mui/material/AppBar";
-import IconButton from "@mui/material/IconButton";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import {Menu} from '@mui/icons-material';
-import LinearProgress from "@mui/material/LinearProgress";
 import TodoListsList from "../TodoList/TodoListsList";
 import {useSelector} from "react-redux";
 import {AppRootStateType, useAppDispatch} from "./store";
-import {APPStateType, RequestStatusType} from "./app-reducer";
 import {ErrorSnackbar} from "../components/ErrorSnackbar /ErrorSnackbar";
 import {Login} from "../Login/Login";
 import {Navigate, Route, Routes} from "react-router-dom";
-import {authReducerStateType, logOutTC, meTC} from "../Login/auth-reducer";
+import {meTC} from "../Login/auth-reducer";
+import {ButtonAppBar} from "../components/ButtonAppBar";
 
 function App() {
-    const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
-    const isInitialized = useSelector<AppRootStateType, boolean>((state) => state.app.isInitialized)
-    const isLoggedIn = useSelector<AppRootStateType, boolean>((state) => state.auth.isLoggedIn)
+    const isInitialized = useSelector<AppRootStateType, boolean>(state => state.app.isInitialized)
     const dispatch = useAppDispatch()
 
     useEffect(() => {
         dispatch(meTC())
-    }, [])
+    }, [dispatch])
 
     if (!isInitialized) {
         return (
-            <div
-                style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+            <div style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
                 <CircularProgress/>
-            </div>)
-    }
-    
-    const logOut = () => {
-      dispatch(logOutTC())
+            </div>
+        )
     }
 
 
     return (
-        <div className="App">
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <Menu/>
-                    </IconButton>
-                    <Typography variant="h6">
-                        News
-                    </Typography>
-                    {isLoggedIn && <Button color="inherit" onClick={logOut}>Log out</Button>}
-                </Toolbar>
-            </AppBar>
-            {status === 'loading' && <LinearProgress color={'secondary'}/>}
-            <Container fixed>
+        <>
+            <ButtonAppBar/>
+            <Container maxWidth={"lg"}>
                 <Routes>
                     <Route path={'/'} element={<TodoListsList/>}/>
                     <Route path={'/login'} element={<Login/>}/>
@@ -63,7 +39,7 @@ function App() {
                 </Routes>
             </Container>
             <ErrorSnackbar/>
-        </div>
+        </>
     )
 }
 
