c2cbc0a8fbaa658b8a49b6567f8fb02866ebdce2
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-12-14 17:12:16 +0300

    fix logic for ErrorSnackbar

--- src/components/ErrorSnackbar /ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar /ErrorSnackbar.tsx
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert, {AlertProps} from '@mui/material/Alert';
 import {useSelector} from "react-redux";
@@ -7,24 +7,23 @@ import {ErrorType, setAppErrorAC} from "../../app/app-reducer";
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
     props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props}/>;
 });
 
 export const ErrorSnackbar = () => {
-    const error = useSelector<AppRootStateType, ErrorType>((state) => state.app.error)
+    const error = useSelector<AppRootStateType, ErrorType>(state => state.app.error)
     const dispatch = useAppDispatch()
 
     const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
-        if (reason === 'clickaway') {
-            return;
-        }
+        if (reason === 'clickaway') return
+
         dispatch(setAppErrorAC(null))
-    };
+    }
 
     return (
         <Snackbar open={error !== null} autoHideDuration={6000} onClose={handleClose}>
             <Alert onClose={handleClose} severity="error" sx={{width: '100%'}}>
-                Error message 😠{error}
+                Error message 😠 {error}
             </Alert>
         </Snackbar>
     );
